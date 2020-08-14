3bc7086c6cbf809676e476574563768e4cfcc579
irgiam <imuqsit@gmail.com>
2020-08-15 03:38:44 +0700

    belum ada log out

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -5,6 +5,8 @@ class Home extends CI_Controller {
 	public function __construct(){
 		parent::__construct();
 		$this->load->model('Mapel_model');
+		$this->load->model("User_model");
+		if($this->User_model->isNotLogin()) redirect(site_url('Login'));
 	}
 	
 	public function index()
